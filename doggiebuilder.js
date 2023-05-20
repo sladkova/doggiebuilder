fc5f1063d811663869beb842e31ba45ae35298ed
Yaroslav-Pa <yaroslav.roro@gmail.com>
2023-05-20 18:42:54 +0300

    builder - validation in build

--- doggiebuilder.js
+++ doggiebuilder.js
@@ -1,45 +1,75 @@
-const nameValidationRegex = /^[а-яіґїє ,.'-]+$/i
-const phoneValidationRegex =  /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+const nameValidationRegex = /^[а-яіґїє ,.'-]+$/i;
+const phoneValidationRegex =
+  /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
 
 class DoggieBuilder {
-    id = ''; 
-    name = '';
-    height = 0;
-    weight = 0;
-    phone = '';
-    owner = '';
-    favoriteToy = '';
+  id = "";
+  name = "";
+  height = 0;
+  weight = 0;
+  phone = "";
+  owner = "";
+  favoriteToy = "";
 
-    constructor(name) {
-        this.name = name;
+  constructor(name) {
+    this.name = name;
+  }
+
+  //опишіть setters для полів класу
+
+  setHeight(height) {
+    this.height = height;
+    return this;
+  }
+
+  setWeight(weight) {
+    this.weight = weight;
+    return this;
+  }
+
+  setPhone(phone) {
+    this.phone = phone;
+    return this;
+  }
+
+  setOwner(owner) {
+    this.owner = owner;
+    return this;
+  }
+
+  setFavoriteToy(favoriteToy) {
+    this.favoriteToy = favoriteToy;
+    return this;
+  }
+
+  build() {
+    // опишіть функцію, використайте nameValidationRegex і phoneValidationRegex
+
+    if (!nameValidationRegex.test(this.name)) {
+      throw new Error("Name is not valid");
     }
-
-   //опишіть setters для полів класу
-
-    build() {
-      // опишіть функцію, використайте nameValidationRegex і phoneValidationRegex
+    if (!phoneValidationRegex.test(this.phone) & (this.phone.length >= 1)) {
+      throw new Error("phone is not valid");
     }
+    return this;
+  }
 }
 
-const dog1 = new DoggieBuilder('Шарик')
-    .setHeight(60)
-    .setWeight(30)
-    .build();
+const dog1 = new DoggieBuilder("Шарик").setHeight(60).setWeight(30).build();
 console.log(dog1);
 
-
-
-const dog2 = new DoggieBuilder('Сірко')
-    .setHeight(40)
-    .setWeight(40)
-    .setOwner('Микола')
-    .build();
+const dog2 = new DoggieBuilder("Сірко")
+  .setHeight(40)
+  .setWeight(40)
+  .setPhone("")
+  .setOwner("Микола")
+  .build();
 console.log(dog2);
 
-const dog3 = new DoggieBuilder('Чорнищ')
-    .setHeight(50)
-    .setWeight(20)
-    .setOwner('Ярослав')
-    .setPhone('0687755643')
-    .build();
+const dog3 = new DoggieBuilder("Чорнищ")
+  .setHeight(50)
+  .setWeight(20)
+  .setOwner("Ярослав")
+  .setPhone("0687755643")
+  .build();
 console.log(dog3);
