64ff2449478c3212edc224d48f5c89d703c67264
MicHaeLNgSd <koratgorovich2017@gmail.com>
2023-05-23 18:49:55 +0300

    DoggieBuilder_Homework

--- doggiebuilder.js
+++ doggiebuilder.js
@@ -1,8 +1,8 @@
 const nameValidationRegex = /^[а-яіґїє ,.'-]+$/i
-const phoneValidationRegex =  /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+const phoneValidationRegex = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
 
 class DoggieBuilder {
-    id = ''; 
+    id = '';
     name = '';
     height = 0;
     weight = 0;
@@ -14,10 +14,37 @@ class DoggieBuilder {
         this.name = name;
     }
 
-   //опишіть setters для полів класу
+    //опишіть setters для полів класу
+    setHeight(height) {
+        this.height = height;
+        return this;
+    }
+    setWeight(weight) {
+        this.weight = weight;
+        return this;
+    }
+    setPhone(phone) {
+        this.phone = phone;
+        return this;
+    }
+    setOwner(owner) {
+        this.owner = owner;
+        return this;
+    }
+    setFavoriteToy(favoriteToy) {
+        this.favoriteToy = favoriteToy;
+        return this;
+    }
 
     build() {
-      // опишіть функцію, використайте nameValidationRegex і phoneValidationRegex
+        // опишіть функцію, використайте nameValidationRegex і phoneValidationRegex
+        if (!nameValidationRegex.test(this.name)) {
+            throw new Error('Name is not valid')
+        }
+        if (!phoneValidationRegex.test(this.phone) && this.phone !== '') {
+            throw new Error('Phone is not valid')
+        }
+        return this
     }
 }
 
@@ -26,8 +53,6 @@ const dog1 = new DoggieBuilder('Шарик')
     .setWeight(30)
     .build();
 console.log(dog1);
-
-
 
 const dog2 = new DoggieBuilder('Сірко')
     .setHeight(40)
@@ -43,3 +68,10 @@ const dog3 = new DoggieBuilder('Чорнищ')
     .setPhone('0687755643')
     .build();
 console.log(dog3);
+
+const dog4 = new DoggieBuilder('Білгородик') //Білгородик1337   //Error by name example
+    //.setPhone('')                                             //Error by phone example
+    //.setPhone('068+775+5643')                                 //Error by phone example
+    .setPhone('(123.456-7890')
+    .build();
+console.log(dog4);
